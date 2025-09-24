525ea2d8f759f077be69479eba7798477edddeae
korwdrah <2817659572@qq.com>
2025-09-24 20:49:03 +0800

    update: 修复分页查询传入值为空不能动态构建sql的问题

--- sky-take-out/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.sky.service.impl;
 import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.sky.constant.MessageConstant;
@@ -74,7 +75,7 @@ public class EmployeeServiceImpl extends ServiceImpl<EmployeeMapper, Employee> i
     public PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO) {
         //分页查询
         Page<Employee> page = new Page<>(employeePageQueryDTO.getPage(), employeePageQueryDTO.getPageSize());
-        Page<Employee> employeePage = employeeMapper.selectPage(page, new LambdaQueryWrapper<Employee>().like(Employee::getUsername, employeePageQueryDTO.getName()));
+        Page<Employee> employeePage = employeeMapper.selectPage(page, new LambdaQueryWrapper<Employee>().like(StringUtils.isNotBlank(employeePageQueryDTO.getName()),Employee::getUsername, employeePageQueryDTO.getName()));
         //返回结果，包装成Result
         return new PageResult(employeePage.getTotal(), employeePage.getRecords());
     }
