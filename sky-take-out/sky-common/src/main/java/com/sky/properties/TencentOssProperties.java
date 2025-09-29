6c5c57fcbf9ca332a57203724b148bdf5ea67acd
korwdrah <2817659572@qq.com>
2025-09-29 21:05:18 +0800

    add: 新增腾讯云OSS的配置和依赖; but: 没有改工具类获取客户端对象

--- sky-take-out/sky-common/src/main/java/com/sky/properties/TencentOssProperties.java
+++ sky-take-out/sky-common/src/main/java/com/sky/properties/TencentOssProperties.java
@@ -5,13 +5,13 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "sky.alioss")
+@ConfigurationProperties(prefix = "sky.tencentoss")
 @Data
-public class AliOssProperties {
+public class TencentOssProperties {
 
-    private String endpoint;
-    private String accessKeyId;
-    private String accessKeySecret;
+    private String secretId;
+    private String secretKey;
     private String bucketName;
+    private String region;
 
 }
