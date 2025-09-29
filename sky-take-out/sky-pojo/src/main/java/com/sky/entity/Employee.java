22effa5f257fcafb54d11385e8b2b15076372c8a
korwdrah <2817659572@qq.com>
2025-09-29 20:45:11 +0800

    update: 通过MP把多个表的id改为默认自增

--- sky-take-out/sky-pojo/src/main/java/com/sky/entity/Employee.java
+++ sky-take-out/sky-pojo/src/main/java/com/sky/entity/Employee.java
@@ -1,7 +1,9 @@
 package com.sky.entity;
 
 import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.*;
 
 import java.io.Serializable;
@@ -14,7 +16,7 @@ import java.time.LocalDateTime;
 public class Employee implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
+    @TableId(type = IdType.AUTO)
     private Long id;
 
     private String username;
